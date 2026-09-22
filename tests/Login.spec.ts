69fa46cc02c80607dc2f4708eb834675e88e0d4f
Mansideshman <mdeshman0429@gmail.com>
2026-09-22 14:21:44 +0530

    Initial Playwright setup with SauceDemo login tests

---
+++ tests/Login.spec.ts
@@ -0,0 +1,29 @@
+import {test, expect } from 'playwright/test';
+
+test('valid login goes to inverntory page',async ({page})=>
+{
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button',{name:'Login'}).click();
+    await expect(page).toHaveURL(/inventory/);
+});
+
+// test('invalid login shows error message',async ({page})=>
+// {
+//     await page.goto('https://www.saucedemo.com/');
+//     await page.getByPlaceholder('Username').fill('standard_user');
+//     await page.getByPlaceholder('Password').fill('wrong password');
+//     await page.getByRole('button',{name:'Login'}).click();
+//     await expect(page.getByText('Username and password do not match')).toBeVisible();
+// });
+
+test('invalid login shows error message',async ({page})=>
+{
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByPlaceholder('Username').fill('locked_out_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+    await page.getByRole('button',{name:'Login'}).click();
+    await expect(page.getByText('locked out')).toBeVisible();
+});
+
